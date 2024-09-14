ef55e9704317c65e2d3df1b88312433048e331f6
Divyansh Anand <57003965+divyansh0908@users.noreply.github.com>
2024-09-14 13:41:51 +0530

    fix: Disable Up and Down button for top and bottom floor respectively

--- index.js
+++ index.js
@@ -65,19 +65,20 @@ function createFloor(floorNumber) {
   new_hr.className = "hr";
   new_hr.id = "hr" + floorNumber;
 
-  // only add down button if floor number is not 1
-  if (floorNumber !== 1) {
-    floorDiv.appendChild(downButton);
+  // Disable the Up button if the floor is the top floor
+  if (floorNumber === parseInt(no_of_floors)) {
+    upButton.disabled = true;
   }
 
-  // only add up button if floor number is not the last floor
-  if (floorNumber !== parseInt(no_of_floors)) {
-    console.log('::::::', { floorNumber, no_of_floors})
-    floorDiv.appendChild(upButton);
+  // Disable the Down button if the floor is the bottom floor
+  if (floorNumber === 1) {
+    downButton.disabled = true;
   }
 
-   // Add rest all elements to the floor
+  // Add all elements to the floor
+  floorDiv.appendChild(upButton);
   floorDiv.appendChild(document.createElement("br"));
+  floorDiv.appendChild(downButton);
   floorDiv.appendChild(floorLabel);
   floorDiv.appendChild(new_hr);
 
